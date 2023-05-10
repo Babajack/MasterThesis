e19c563323c61005f64b71e2f9d70e34cb186b76
Babajack <p.heldmann@yahoo.de>
2023-05-10 12:01:53 +0200

    added first structure

---
+++ src/components/TaskComponent.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const TaskComponent = () => {
+    return <>test</>
+}
+
+export default TaskComponent;
---
+++ src/views/TaskView.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const TaskView = () => {
+    return <>test</>
+}
+
+export default TaskView;
